2ba14654a717b6279120911787fa17e95f3ed0f3
Wardium <84645455+Wardium@users.noreply.github.com>
2026-03-28 09:38:54 -0700

    Updated Models

--- aurora.py
+++ aurora.py
@@ -97,8 +97,8 @@ Convert = False
 Continue = ""
 tts = TTS("tts_models/en/ljspeech/tacotron2-DDC")
 
-ai_model_name = "gemini-1.5-flash"
-fast_ai_model_name = "gemini-1.5-flash-8b"
+ai_model_name = "gemini-2.5-flash"
+fast_ai_model_name = "gemini-2.5-flash"
 
 
 personality = settings.personality_text + (
